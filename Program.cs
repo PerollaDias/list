45c081b43aaaa29c89dc2893035b59a9552563ee
Perolla <perolladiasmoreira@gmail.com>
2023-07-27 11:52:10 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+class array
+{
+    static void Main(string[] args)
+    {
+        int[] nota = new int[3];
+        Console.WriteLine("Digite a nota um");
+        nota[0] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a nota dois");
+        nota[1] = Convert.ToInt32(Console.ReadLine());
+
+        nota[2] = (nota[0] + nota[1]) / 2;
+
+        int resultado = nota[2];
+        Console.WriteLine(resultado);
+    }
+    
+}
